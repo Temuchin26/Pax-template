f41a2e24f8046469b97bf6688476121a83bc38b8
Temuchin <temuchin26@gmail.com>
2017-11-11 22:57:29 +0300

    First commit1

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,29 @@
-(function(){
+;(function(){
 
-  console.log('GULP');
+  var w = $('.slaider__cont').width();
+  $('.slaider__items').width(w * $('.slaider__item').length);
+  $('.slaider__items').css('left', -w);
+
+  $('.slaider__item:last-child').prependTo('.slaider__items');
+
+  $('#nextSlide').click(function(){
+    $('.slaider__items').animate({
+      'margin-left': -w
+    }, 500, function(){
+      $('.slaider__items').css('margin-left', 0);
+      $('.slaider__item:first-child').appendTo('.slaider__items');
+    });
+  });
+
+  $('#prevSlide').click(function(){
+    $('.slaider__items').animate({
+      'margin-left': w
+    }, 500, function(){
+      $('.slaider__items').css('margin-left', 0);
+      $('.slaider__item:last-child').prependTo('.slaider__items');
+    });
+  });
+
+
 
 }());
